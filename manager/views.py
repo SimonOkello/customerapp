6d999bdbdb59265876a4ad7a505ec8e512da79ca
SimonOkello <simonokello93@gmail.com>
2020-07-12 07:24:06 +0300

    Added models and querysets

--- manager/views.py
+++ manager/views.py
@@ -1,11 +1,25 @@
 from django.shortcuts import render
+from .models import Product, Tag, Customer, Order
 
 # Create your views here.
+
+
 def index(request):
-    return render(request, 'manager/index.html', {})
+    customers = Customer.objects.all()
+    orders = Order.objects.all()
+    customers_total = customers.count()
+    orders_total = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {'customers': customers, 'orders': orders, 'customers_total': customers_total,
+               'orders_total': orders_total, 'delivered': delivered, 'pending': pending}
+    return render(request, 'manager/index.html', context)
+
 
 def customer(request):
     return render(request, 'manager/customer.html', {})
 
+
 def product(request):
-    return render(request, 'manager/products.html', {})
+    products = Product.objects.all()
+    return render(request, 'manager/products.html', {'products': products})
